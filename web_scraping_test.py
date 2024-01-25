5c23ca3d2c61b5a03c49d2b0f2847a0a0bdf287e
Rafael Campos Oviedo <104372963+rcampos1970@users.noreply.github.com>
2024-01-25 14:49:11 -0300

    Update web_scraping_test.py

--- web_scraping_test.py
+++ web_scraping_test.py
@@ -6,11 +6,8 @@ link = "https://seatable.io/en/online-datenbank-kostenlos/"
 requisicao = requests.get(link)
 site = BeautifulSoup(requisicao.text,"html.parser")
 tag = "link"
-file_name = "example.txt"
+file_name = "links.txt"
 with open(file_name, 'w') as file:
     for variavel in site.find_all(tag):
         print(variavel["href"])
         file.write(variavel["href"])
-
-#list = variavel[5]
-#print(list.text)
